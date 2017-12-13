0e9d8fa44d270457735ebb0b7afbd5b9690643de
jason <jason.ch.ross@gmail.com>
2017-12-13 09:28:51 -0500

    Lecture 70 - Resource Creation Endpoint - POST/todos * Setup server.js to import mongoose, express and bady-parser * Setup a POST route to create new Todos * Tested with postman

--- server/server.js
+++ server/server.js
@@ -1,65 +1,31 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)  => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-//User model (email, password)
 
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-var newUser = new User({
-    email: 'jason@example.com'
-});
-
-newUser.save().then((doc)=>{
-    console.log('Saved user model', doc)
-}, (e)=>{
-    console.log('Unable to save', e)
-});
-
-/*
-
-var newTodo = new Todo({
-    text: 'Cook dinner'
-});
-
-newTodo.save().then((doc) => {
-    console.log('Saved todo', doc)
-}, (e) => {
-    console.log('Unable to save');
-});*/
-
-
-/*var otherTodo = new Todo({
-    text: '   Edit this document    '
-});
-
-otherTodo.save().then((doc) => {
-    console.log('Saved todo', doc)
-}, (e) => {
-    console.log('Unable to save', e);
-});*/
