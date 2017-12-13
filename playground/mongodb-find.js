76747ba581dd56c3d8412896f2c4bec91c7f938e
jason <jason.ch.ross@gmail.com>
2017-12-12 23:53:44 -0500

    Lecture 66 - Deleting Documents * Basic delete method usage: deleteOne, deleteMany, findOneAndDelete

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -17,7 +17,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
         console.log('Connected to MongoDB server');
 
-/*    var docs = db.collection('Todos').find({
+/*
+   var docs = db.collection('Todos').find({
         _id: new ObjectID('5a2fe6d91de176467726baa9')
     }).toArray().then((docs) => {
         console.log('Todos');
@@ -25,16 +26,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         db.close();
     }, (err) =>{
         console.log('Unable to fetch todos', err);
-    });*/
+    });
+*/
 
 
 
-/*    db.collection('Todos').find().count().then((count) => {
+/*
+    db.collection('Todos').find().count().then((count) => {
         console.log(`Todos count ${count}`);
         db.close();
     }, (err) =>{
         console.log('Unable to fetch todos', err);
-    });*/
+    });
+
+*/
 
     var docs = db.collection('Users').find({
         name: 'Hb'
