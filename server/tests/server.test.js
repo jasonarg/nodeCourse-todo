a6059e2599c43cf6fd954f7edac41c60f54f00d6
jason <jason.ch.ross@gmail.com>
2017-12-14 11:03:38 -0500

    Lecture 83 - Testing Delete a Resource DELETE /todos/:id * Added three mocha test cases for deleting todos by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,8 +98,49 @@ describe('GET /todos/:id', () =>{
         request(app)
             .get(`todos/1231231231`)
             .expect(404)
-            .end(done)
-        });
+            .end(done);
+    });
 
 
+});
+
+
+describe('DELETE /todos/:id', () =>{
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        console.log(hexId);
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                //query db using findById toNotExist assertion
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+
+            });
+
+    });
+
+    it('should return a 404 if todo not found', (done) =>{
+        request(app)
+            .delete(`todos/${todos[0]._id.toHexString().replace('a', 'c')}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+            .delete(`todos/1231231231`)
+            .expect(404)
+            .end(done);
+    });
 });
