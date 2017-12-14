0de5b4af6000285237116fb4b8459ebeed9f9a55
jason <jason.ch.ross@gmail.com>
2017-12-14 12:44:01 -0500

    Section 8 - User authentication, Lecture 88 - Setting up the User Model * Updated models/user.js to include validator library for validating the email address of user * Added additional properties on user * Created a route in server.js to post a new user with basic error handling

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
 
 
 var User = mongoose.model('User', {
@@ -6,8 +8,30 @@ var User = mongoose.model('User', {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 10
+    },
+    tokens: [
+        {
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 });
 
 module.exports = {User};
